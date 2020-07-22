43cb17bf0cd9cd29b49d43863034c762f403ca0a
Atul Kashyap <57857365+Atul-Kashyap@users.noreply.github.com>
2020-07-22 12:10:32 +0530

    Update and rename Maximum_Depth_of_Binary_Tree to Maximum_Depth_of_Binary_Tree.cpp

--- Maximum_Depth_of_Binary_Tree.cpp
+++ Maximum_Depth_of_Binary_Tree.cpp
@@ -12,8 +12,7 @@ Space Complexity : O(H)
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
- 
+ */ 
 class Solution {
 public:
     int maxDepth(TreeNode* root) {

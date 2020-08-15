a9c5a4915c5513eec1aef514680a2aed953554fb
Atul Kashyap <57857365+Atul-Kashyap@users.noreply.github.com>
2020-08-15 19:31:45 +0530

    Update Minimum_Depth_of_Binary_Tree.cpp

--- Minimum_Depth_of_Binary_Tree.cpp
+++ Minimum_Depth_of_Binary_Tree.cpp
@@ -24,3 +24,39 @@ public:
     return 1 + (min(L, R) ? min(L, R) : max(L, R));
     }
 };
+
+
+Approach 2: iterative using BFS
+Time Complexity : O(H);
+Auxiliary Space : O(H), H = height of tree;
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        int depth = 1;
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            if(!curr){
+                q.push(NULL);
+                depth++;
+            }
+            else{
+                if( !curr->left && !curr->right)
+                    return depth;
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            
+        }
+        return depth;
+       
+    }
+};

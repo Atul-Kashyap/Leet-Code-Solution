2b25825d3e59be3a7584411677ee73e546c6a744
Atul Kashyap <atulkashyap1208@gmail.com>
2020-09-24 18:12:55 +0530

    Level - Easy, Linked List

---
+++ Remove Linked List Elements.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Approach 1: Sentinel
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+    
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* sentinel = new ListNode(0);
+        sentinel -> next = head;
+        ListNode *prev = sentinel, *curr = head, *todelete = NULL;
+        while(curr != NULL){
+            
+            if(curr -> val == val){
+                prev -> next = curr -> next;
+                todelete = curr;
+            } 
+            else 
+                prev = curr;
+            
+            curr = curr -> next;
+            
+            if(todelete != NULL){
+                delete todelete;
+                todelete = NULL;
+            }
+            
+        }
+        return sentinel->next;
+    }
+};

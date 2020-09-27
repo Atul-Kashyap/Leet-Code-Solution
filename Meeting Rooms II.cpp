11d39aaca6d289f69fc3c409c9ea7b847370fcea
Atul Kashyap <atulkashyap1208@gmail.com>
2020-09-27 17:04:11 +0530

    Level - Medium, Priority Queue and Scheduling Algorithms

---
+++ Meeting Rooms II.cpp
@@ -0,0 +1,70 @@
+// Approach 1: Priority Queue
+// Time Complexity :(NLogN)
+// Space Complexity : O(N)
+
+
+class Solution {
+public:
+    static bool comp(vector<int> a, vector<int> b){
+        return a[0] < b[0];
+        
+    }
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        int sz = intervals.size();
+        if(sz == 0)
+            return 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int room = 1;
+        sort(intervals.begin(), intervals.end(), comp);
+        pq.push(intervals[0][1]);
+        for(int i = 1; i < sz; i++){
+            int end_time = pq.top();
+            if(intervals[i][0] >= end_time){
+                pq.pop();
+                pq.push(intervals[i][1]);
+            }
+            else{
+                room++;
+                pq.push(intervals[i][1]);
+            }
+        }
+       return room; 
+    }
+};
+
+// Approach 2: Chronological Ordering
+// Time Complexity :(NLogN)
+// Space Complexity : O(N)
+
+
+
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        int sz = intervals.size();
+        if(sz == 0)
+            return 0;
+        vector<int> start(sz);
+        vector<int> end(sz);
+        for(int i = 0; i < sz; ++i){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        
+        int s_ptr = 0, e_ptr = 0;
+        int usedrooms = 0;
+        while(s_ptr < sz){
+            if(start[s_ptr] >= end[e_ptr]){
+                usedrooms -= 1;
+                e_ptr += 1;
+            }
+            
+            usedrooms += 1;
+            s_ptr += 1;
+        }
+        
+        return usedrooms;
+    }
+};
